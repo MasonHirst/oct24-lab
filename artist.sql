fe506699ad9ad19669ddb476ef4c81fcf08cff63
Mason Hirst <mason.j.hirst@gmail.com>
2022-10-25 09:43:23 -0600

    finished artist.sql

---
+++ artist.sql
@@ -0,0 +1,23 @@
+1. INSERT INTO artist (name)
+    VALUES ('Max Hirst'),
+    ('Ur Mom'),
+    ('Young Gravy')
+
+2. SELECT name
+    FROM artist
+    ORDER BY name DESC
+    LIMIT 10;
+
+3. SELECT name
+    FROM artist
+    LIMIT 5;
+
+4. SELECT name
+    FROM artist
+    WHERE name LIKE 'Black%';
+
+5. SELECT name
+    FROM artist
+    WHERE name LIKE '%Black%';
+
+
