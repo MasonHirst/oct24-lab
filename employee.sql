b454da23106472cfdc4f7c163172f078a0b4645b
Mason Hirst <mason.j.hirst@gmail.com>
2022-10-26 11:08:48 -0600

    finished most of employees.sql

---
+++ employee.sql
@@ -0,0 +1,12 @@
+1. select last_name, first_name, city from employee
+    where city = 'Calgary' ;
+
+2. select * from employee
+    order by birth_date DESC
+    Limit 1;
+
+3. select * from employee
+    order by birth_date
+    Limit 1;
+
+4.  nancy is 2
